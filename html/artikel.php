5ca5dbce2a107586bf92cf78558428d09df2ed1b
Unknown <rogg.marvin@t-online.de>
2019-05-16 10:53:20 +0200

    Navbar verbessert

--- html/artikel.php
+++ html/artikel.php
@@ -20,34 +20,35 @@
 
 <body>
 
+
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <a class="navbar-brand" href="#">Cemquarium</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
+                    <a class="nav-link" href="index.php">Startseite
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="ueberuns.php">Über uns</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
+                    <a class="nav-link" href="kontakt.php">Kontakt</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link" href="login.php">Login</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Logout</a>
+                    <a class="nav-link" href="logout.php">Logout</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Warenkorb</a>
+                    <a class="nav-link" href="warenkorb.php">Warenkorb</a>
                 </li>
 
             </ul>
@@ -57,7 +58,9 @@
         </form>
     </div>
 </nav>
-  <!-- Page Content -->
+
+
+<!-- Page Content -->
   <div class="container">
 
     <div class="row">
