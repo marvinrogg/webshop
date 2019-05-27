a84c08535e6b65d8e38cf7734823b0a4081b2e1c
marvinrogg <rogg.marvin@t-online.de>
2019-05-27 18:32:10 +0200

    PHP, Artikelansicht
    Artikel werden auf der Startseite aus der Datenbank gelesen

--- html/index.php
+++ html/index.php
@@ -114,53 +114,10 @@ include 'isLoggedIn.php';
 
         <div class="row">
 
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card mt-4">
-              <a href="#"><img class="card-img-top" src="/webshop/pictures/lobster.jpg" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">Item One</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">**Produktbeschreibung**</p>
-                <?php
-                include 'addtowarenkorb.php';
-                ?>
-              </div>
-            </div>
-          </div>
 
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card mt-4">
-              <a href="#"><img class="card-img-top" src="/webshop/pictures/lobster.jpg" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">Item Two</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">**Produktbeschreibung**</p>
-                  <?php
-                  include 'addtowarenkorb.php';
-                  ?>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card mt-4">
-              <a href="#"><img class="card-img-top" src="/webshop/pictures/lobster.jpg" alt=""></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">Item Three</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">**Produktbeschreibung**</p>
-                  <?php
-                  include 'addtowarenkorb.php';
-                  ?>
-              </div>
-            </div>
-          </div>
+            <?php
+            include 'readitemfromdb.php'
+            ?>
 
 
         </div>
---
+++ html/readitemfromdb.php
@@ -0,0 +1,38 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "Cemquarium");
+if ($mysqli->connect_error){
+    die("Verbindung zur DB fehlgeschlagen: " . $mysqli->connect_error);
+}
+$id = "1";
+$sql = "SELECT * FROM Produkt
+        WHERE ?;";
+
+$statement= $mysqli->prepare($sql);
+$statement->bind_param('s', $id);
+$statement->execute();
+$result = $statement->get_result();
+
+while($row = $result->fetch_object()) {
+    $name = $row->name;
+    $beschreibung = $row->beschreibung;
+    $preis = $row->preis;
+
+    echo '<div class="col-lg-4 col-md-6 mb-4">
+            <div class="card mt-4">
+              <a href="#"><img class="card-img-top" src="/webshop/pictures/lobster.jpg" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">'.$name.'</a>
+                </h4>
+                <h5>'.$preis.'€</h5>
+                <p class="card-text">'.$beschreibung.'</p>';
+
+                  include 'addtowarenkorb.php';
+     echo '             
+              </div>
+            </div>
+          </div>';
+}
+
+//$numberofarticel = $result->num_rows;
+// for ($i=0; $i < $numberofarticel; $i++){}
