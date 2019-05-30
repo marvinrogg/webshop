fe25333a27f0f495c0e741dd2ad04b52f7895609
marvinrogg <rogg.marvin@t-online.de>
2019-05-30 14:13:47 +0200

    PHP; Bildanzeige
    Bilder werden jetzt korrekt angezeigt

--- html/readpumpenfromdb.php
+++ html/readpumpenfromdb.php
@@ -15,17 +15,18 @@ while ($row = $result->fetch_object()) {
     $name = $row->name;
     $beschreibung = $row->beschreibung;
     $preis = $row->preis;
+    $bild = $row->bild;
+    // print $bild;
 
     echo '<div class="col-lg-4 col-md-6 mb-4">
             <div class="card mt-4">
-              <a href="#"><img class="card-img-top" src="/webshop/pictures/lobster.jpg" alt=""></a>
+              <a href="#"><img class="card-img-top" src="' . $bild . '" alt=""></a>
               <div class="card-body">
                 <h4 class="card-title">
                   <a href="#">' . $name . '</a>
                 </h4>
                 <h5>' . $preis . '€</h5>
                 <p class="card-text">' . $beschreibung . '</p>';
-
     if (isset($_SESSION['username'])) {
         // ist eingeloggt
         $benutzer = $_SESSION['username'];
