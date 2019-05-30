07ac89692dd88b92d8e5863ecd953922a4a93789
CemDabanli <edabanli@hs-offenburg.de>
2019-05-30 15:40:20 +0200

    Insert/Delete-Warenkorb

--- Datenbank/sqlAbfragen.sql
+++ Datenbank/sqlAbfragen.sql
@@ -26,7 +26,8 @@ where Unterkategorie_id =1
 OR Unterkategorie_id =2
 ;
 
+
 INSERT INTO `Cemquarium`.`Warenkorb` (`id`, `User_username`) VALUES (1, 'Cem');
-
-
 INSERT INTO `Cemquarium`.`Warenkorb_has_Produkt` (`Warenkorb_id`, `Produkt_id`) VALUES (1, 1);
+DELETE FROM Warenkorb_has_Produkt WHERE Produkt_id=1;
+DELETE FROM Warenkorb WHERE id=1;
